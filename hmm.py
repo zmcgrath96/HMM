abc6a69388d776ddc9656f8567b737a395c50470
Kevin <kevin_ray@ku.edu>
2019-03-20 17:07:23 -0500

    2nd try at hmm based off of wikipedia formulas

--- hmm.py
+++ hmm.py
@@ -2,125 +2,180 @@ import string
 import numpy as np
 import copy
 import math
+import sys
+from queue import Queue
+import threading
+import time
 
 class HMM:
     def __init__(self, states, uniqueWords):
-        self.states = states
-        self.observations = len(uniqueWords)
-        self.transition = np.full((self.states, self.states), 1/self.states)
-        self.emmission = np.full((self.states, self.observations), 1/self.observations)
-        self.initialization = np.full((self.states, 1), 1/self.states)
+        self.numStates = states
+        self.numObvs = len(uniqueWords)
+        self.a = np.random.rand(self.numStates, self.numStates)
+        self.b = np.zeros((self.numStates, self.numObvs))
+        self.pi = np.random.rand(self.numStates, 1)
         self.word_map = self.make_word_map(uniqueWords)
+        self.index_map = self.make_index_map()
+        # fix a
+        for i in range(self.numStates):
+            sum = np.sum(self.a[i,:])
+            for j in range(self.numStates):
+                self.a[i,j] = self.a[i,j] / sum
+
+        # fix pi
+        self.pi = self.pi / np.sum(self.pi)
+
+        #fill b
+        sum = 0
+        for i in range(self.numStates):
+            for word in uniqueWords:
+                self.b[i, self.word_index(word)] = uniqueWords[word]
+                sum += uniqueWords[word]
+        self.b = self.b / sum
 
     def trainHMM(self, textCorpus, maxIter=1000, threshold=0.00001):
-
-        for i in range(maxIter):
-            # E - step
+        text_by_index = self.text_to_index(textCorpus)
+        for it in range(maxIter):
+            start = time.time()
+            print("Iteration {}".format(it))
+            # save old stuff
+            old_a = copy.copy(self.a)
+            old_b = copy.copy(self.b)
+            old_pi = copy.copy(self.pi)
             gammas = []
-            chis = []
-            for sentence, n in zip(textCorpus, range(len(textCorpus))):
-                if len(sentence) < 3:
-                    continue
-                print("Iteration: {}, Percent Complete: {:.2f}%".format(i, 100 * n / len(textCorpus)), end='\r', flush=True)
-                alphas = self.forwardBaumWelch(sentence)
-                betas = self.backwardBaumWelch(sentence)
-                gammas.append(self.getGammas(alphas, betas, sentence))
-                chis.append(self.getChis(alphas, betas, sentence))
+            xis = []
 
-            # save old copies of matrices commparison
-            prvTransition = copy.copy(self.transition)
-            prvEmmision = copy.copy(self.emmission)
-            prvInitilization = copy.copy(self.initialization)
+            # put all lines in queue
+            for seq, n in zip(text_by_index, range(len(text_by_index))):
+                if n % 10000 == 0:
+                    print("Line {}".format(n))
+                alpha = self.forwardBaumWelch(seq)
+                beta = self.backwardBaumWelch(seq)
+                gammas.append(self.getGamma(alpha, beta, seq))
+                xis.append(self.getXi(alpha, beta, seq))
 
-            # M - step
-            # update initialization
-            for i in range(self.states):
-                for t in range(len(textCorpus)):
-                    self.initialization[i,0] = sum(gammas[t][0][i])
-            self.initialization = self.initialization / (len(textCorpus))
+            print("finished e-step")
 
-            # update transition
-            for i in range(self.states):
-                for j in range(self.states):
-                    chiSums = 0
-                    gammaSums = 0
-                    for sentence, T in zip(textCorpus, range(len(textCorpus))):
-                        for t in range(len(sentence) - 1):
-                            chiSums += chis[T][t][i,j]
-                            gammaSums += gammas[T][t][i]
-                    self.transition[i,j] = chiSums / gammaSums
+            # maximization
 
-            # update emmission
-            for i in range(self.states):
-                for word in range(len(self.emmission)):
-                    numGammaSum = 0
-                    denGammaSum = 0
-                    for sentence, T in zip(textCorpus, range(len(textCorpus))):
-                        for t in range(len(sentence)):
-                            if self.word_index(sentence[t]) == word:
-                                numGammaSum += gammas[T][t][i]
-                            denGammaSum += gammas[T][t][i]
-                    self.emmission[i, word] = numGammaSum / denGammaSum
+            # pi
+            print("maxing pi")
+            self.pi = np.zeros((self.numStates, 1))
+            for i in range(self.numStates):
+                for T in range(len(textCorpus)):
+                    self.pi[i,0] += gammas[T][i,0]
+            sum = np.sum(self.pi)
+            self.pi[:,0] = self.pi[:,0] / sum
 
+            # a
+            print("maxing a")
+            for i in range(self.numStates):
+                denom = 0
+                for seq, T in zip(text_by_index, range(len(text_by_index))):
+                    denom += np.sum(gammas[T][i,0:-2])
 
-            diffTransition = np.absolute(np.linalg.norm(self.transition, ord='fro') -
-                                         np.linalg.norm(prvTransition, ord='fro'))
-            diffEmmission = np.absolute(np.linalg.norm(self.emmission, ord='fro') -
-                                         np.linalg.norm(prvEmmision, ord='fro'))
-            diffInitialization = np.absolute(np.linalg.norm(self.initialization, ord='fro') -
-                                         np.linalg.norm(prvInitilization, ord='fro'))
-            totalDiff = diffTransition + diffEmmission + diffInitialization
-            print("Diff A: " + str(diffTransition))
-            print("Diff B: " + str(diffEmmission))
-            print("Diff I: " + str(diffInitialization))
-            if totalDiff < threshold:
+                for j in range(self.numStates):
+                    numer = 0
+                    for seq, T in zip(text_by_index, range(len(text_by_index))):
+                        numer += np.sum(xis[T][i,j,0:-2])
+                    self.a[i,j] = numer / denom
+
+            # b
+            print("maxing b")
+            self.b = np.zeros((self.numStates, self.numObvs))
+            for i in range(self.numStates):
+                denom = 0
+                for seq, T in zip(text_by_index, range(len(text_by_index))):
+                    denom += np.sum(gammas[T][i,:])
+                    for word, t in zip(seq, range(len(seq))):
+                        self.b[i, word] += gammas[T][i,t]
+
+                self.b[i,:] = self.b[i,:] / denom
+
+            print("done maxing")
+            diffA = np.absolute(np.linalg.norm(self.a) - np.linalg.norm(old_a))
+            diffB = np.absolute(np.linalg.norm(self.b) - np.linalg.norm(old_b))
+            diffPi = np.absolute(np.linalg.norm(self.pi) - np.linalg.norm(old_pi))
+            total_diff = diffA + diffB + diffPi
+            print("Total diff: {}".format(total_diff))
+            print("Total time: {}".format(time.time() - start))
+            if total_diff <= threshold:
                 break
-        print(self.transition)
-        print(self.emmission)
-        print(self.initialization)
 
-    def forwardBaumWelch(self, sentence):
-        alphas = np.zeros((self.states, len(sentence)))
-        alphas[:, [0]] = self.initialization * self.emmission[:, [self.word_index(sentence[0])]]
-        for t in range(1, len(sentence)):
-            for i in range(self.states):
-                alphas[i, t] = self.emmission[i, [self.word_index(sentence[t])]] * sum(alphas[:,[t-1]] * self.transition[:, [i]])
-        return alphas
+        if self.sanityCheck():
+            return self.a, self.b, self.pi
+        return None, None, None
 
-    def backwardBaumWelch(self, sentence):
-        betas = np.zeros((self.states, len(sentence)))
-        betas[:, -1] = 1
-        for t in range(len(sentence)-2, -1, -1):
-            for i in range(self.states):
-                betas[i, t] = sum(betas[:,[t+1]] * np.transpose(self.transition[[i], :]) * self.emmission[:, [self.word_index(sentence[t+1])]])
-        return betas
 
-    # derived from formula for gamma at:
-    # https://genome.sph.umich.edu/w/images/b/b7/Biostat615-fall2011-lecture20.pdf
-    def getGammas(self, alphas, betas, sentence):
-        gammas = []
-        for t in range(len(sentence)):
-            gammas.append(np.zeros((self.states, 1)))
-            for i in range(self.states):
-                gammas[t][i] = alphas[i,t] * betas[i,t]
-            gammas[t] = gammas[t] / sum(gammas[t])
-        return gammas
+    def sanityCheck(self):
+        for i in range(self.numStates):
+            sumA = 0.0
+            for j in range(self.numStates):
+                sumA += self.a[i,j]
+            if (int(round(sumA)) != 1):
+                print("A: i: {}, sumA: {}".format(i, int(round(sumA))))
+                return False
 
-    # derived from formula for chis at:
-    # https://genome.sph.umich.edu/w/images/b/b7/Biostat615-fall2011-lecture20.pdf
-    def getChis(self, alphas, betas, sentence):
-        chis = []
-        for t in range(len(sentence) - 1):
-            chis.append(np.zeros((self.states, self.states)))
-            denominator = 0
-            for i in range(self.states):
-                for j in range(self.states):
-                    chis[t][i,j] = (alphas[i,t] * self.transition[i,j] *
-                                    self.emmission[j, [self.word_index(sentence[t+1])]] *
-                                    betas[j, t + 1])
-                    denominator += chis[t][i,j]
-            chis[t] = chis[t] / float(denominator)
-        return chis
+        for i in range(self.numStates):
+            sumB = 0.0
+            for j in range(self.numObvs):
+                sumB += self.b[i,j]
+            if (int(round(sumB)) != 1):
+                print("B: i: {}, sumB: {}".format(i, int(round(sumB))))
+                return False
+
+        sumPi = 0.0
+        for i in range(self.numStates):
+            sumPi += self.pi[i,0]
+        if (int(round(sumPi)) != 1):
+            print("Pi: sumPi: {}".format(sumPi))
+            return False
+        return True
+
+
+    def forwardBaumWelch(self, seq):
+        alpha = np.zeros((self.numStates, len(seq)))
+        alpha[:,0] = self.pi[:,0] * self.b[:,seq[0]]
+
+        for t in range(1, len(seq)):
+            for i in range(self.numStates):
+                sum = 0
+                for j in range(self.numStates):
+                    sum += alpha[j,t-1] * self.a[j,i]
+                alpha[i,t] = self.b[i,seq[t-1]] * sum
+        return alpha
+
+
+    def backwardBaumWelch(self, seq):
+        beta = np.zeros((self.numStates, len(seq)))
+        beta[:,-1] = 1
+
+        for t in range(len(seq) - 2, -1, -1):
+            for i in range(self.numStates):
+                for j in range(self.numStates):
+                    beta[i,t] += beta[j,t+1] * self.a[i,j] * self.b[j,seq[t+1]]
+        return beta
+
+    def getGamma(self, alpha, beta, seq):
+        gamma = np.zeros((self.numStates, len(seq)))
+        denom = 0
+        for i in range(self.numStates):
+            for t in range(len(seq)):
+                gamma[i,t] = alpha[i,t] * beta[i,t]
+                denom += gamma[i,t]
+        gamma = gamma / denom
+        return gamma
+
+    def getXi(self, alpha, beta, seq):
+        xi = np.zeros((self.numStates, self.numStates, len(seq)))
+        denom = 0
+        for i in range(self.numStates):
+            for j in range(self.numStates):
+                for t in range(len(seq)-1):
+                    xi[i,j,t] = alpha[i,t] * self.a[i,j] * beta[j,t+1] * self.b[j,seq[t+1]]
+                    denom += xi[i,j,t]
+        xi = xi / denom
+        return xi
 
     # keep track of word integer relationship for numpy arrays
     def make_word_map(self, uniqueWords):
@@ -135,3 +190,20 @@ class HMM:
     # return the integer associated with the word
     def word_index(self, word):
         return self.word_map[word]
+
+    def index_word(self, index):
+        return self.index_map[index]
+
+    def make_index_map(self):
+        map = {}
+        for word in self.word_map:
+            map[self.word_map[word]] = word
+
+    def text_to_index(self, text):
+        text_by_index = []
+        for seq in text:
+            seq_by_index = []
+            for word in seq:
+                seq_by_index.append(self.word_index(word))
+            text_by_index.append(seq_by_index)
+        return text_by_index
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import string
 import hmm
+import numpy as np
 
 def main(args):
 
@@ -8,18 +9,28 @@ def main(args):
     text = ""
     table = str.maketrans("", "", string.punctuation)
     textCorpus = []
-    uniqueWords = set({})
-    for line, n in zip(file, range(100000)):
+    uniqueWords = {}
+
+    for line in file:
         words = line.translate(table).replace("\n", "").lower().split(" ")
+        if len(words) <= 2 or len(words) > 50:
+            continue
         textCorpus.append(words)
         for word in words:
-            uniqueWords.add(word)
-        if n > 50000:
-            break
+            if word not in uniqueWords:
+                uniqueWords[word] = 0
+            uniqueWords[word] += 1
+
     file.close()
 
-    shakeHMM = hmm.HMM(2, uniqueWords)
-    shakeHMM.trainHMM(textCorpus)
+    shakeHMM = hmm.HMM(5, uniqueWords)
+    a, b, pi = shakeHMM.trainHMM(textCorpus, maxIter=1, threshold=0.01)
+    if a is not None and b is not None and pi is not None:
+        np.savetxt("output/a.out", a)
+        np.savetxt("output/b.out", b)
+        np.savetxt("output/pi.out", pi)
+    else:
+        print("Error running hmm")
 
 
 if __name__ == '__main__':
